e893957f1e748888a678b1f9c3fcc5ad802b6b6a
Joshua Laurence <laurence11@fb.com>
2022-10-27 15:04:20 +0100

    Created hover animation for streaming providers

--- src/displayStreamingProviders.js
+++ src/displayStreamingProviders.js
@@ -1,6 +1,6 @@
 
 //List of temporary streaming services for testing purposes
-const handleOnMouseMove = event => {
+const handleOnMouseMoveSearchElement = event => {
     const {currentTarget: target} = event;
     const rect = target.getBoundingClientRect(), x = event.clientX - rect.left, y = event.clientY-rect.top;
     target.style.setProperty("--mouse-x", `${x}px`)
@@ -16,7 +16,7 @@ function displayStreamingServices(retrievedStreamingServices, movieTitle, movieI
     title.innerHTML = movieTitle
     searchResult.appendChild(title)
     searchResult.setAttribute("onclick", `location.href='movieInformation.html?movieID=${movieID}'`)
-    searchResult.onmousemove = event => handleOnMouseMove(event);
+    searchResult.onmousemove = event => handleOnMouseMoveSearchElement(event);
     if (retrievedStreamingServices !== undefined) {
         const iconDiv = document.createElement("div");
         iconDiv.classList.add("theItemDiv")
