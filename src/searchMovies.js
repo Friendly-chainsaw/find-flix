81cc478e01e68d280852f01fecca3a8fd4e47663
Joshua Laurence <laurence11@fb.com>
2022-10-27 13:34:20 +0100

    Created a custom error message for empty search bar

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -12,6 +12,19 @@ window.addEventListener("load", (event) => {
     }
 })
 */
+function createErrorMessage(errorMessage) {
+  const para = document.createElement("p");
+  const node = document.createTextNode(errorMessage
+  );
+  para.setAttribute("id", "error");
+  para.appendChild(node);
+  const element = document.getElementById("movies");
+  element.appendChild(para);
+  setTimeout(() => {
+    document.getElementById("error").remove();
+  }, 1000);
+}
+
 async function searchMovies(searchKeyWord, page) {
   let data = [];
   try {
@@ -31,25 +44,21 @@ async function searchMovies(searchKeyWord, page) {
         data[i].title, data[i].id);
     }
   } catch (error) {
-    const para = document.createElement("p");
-    const node = document.createTextNode(
-      "Oops! Try a different keyword or phrase!"
-    );
-    para.setAttribute("id", "error");
-    para.appendChild(node);
-    const element = document.getElementById("movies");
-    element.appendChild(para);
-    setTimeout(() => {
-      document.getElementById("error").remove();
-    }, 1000);
+    createErrorMessage("Oops! Try another keyword of phrase")
   }
   return data;
 }
 
+
 document.getElementById("search").addEventListener("click", function () {
-  searchMovies(document.getElementById("query").value, 1);
+  const str = document.getElementById("query").value;
+  if (str === null || str.match(/^ *$/) !== null) { createErrorMessage("Oops! The search bar appears to be empty.") }
+  else {searchMovies(str, 1);}
 });
 
+document.getElementById("form").addEventListener("active", () => {
+  document.getElementById("form").classList.toggle("formAnimation")
+})
 // document.getElementById("query").addEventListener("keydown", function () {
 //   if (document.getElementById("query").value.length != 0) {
 //     // doesn't allow app to query the api unless the text box is !empty.
