6a0daa6b466b2e8c491f782325708e93f1cb6420
Joshua Laurence <laurence11@fb.com>
2022-10-28 12:43:22 +0100

    Added core functionality to upcoming movies page

---
+++ src/displayUpcomingMovies.js
@@ -0,0 +1,51 @@
+window.addEventListener("load", (event) => {
+    displayUpcomingMovies(1)
+})
+
+const handleOnMouseMovePaginationBar = event => {
+    const {currentTarget: target} = event;
+    const rect = target.getBoundingClientRect(), x = event.clientX - rect.left, y = event.clientY-rect.top;
+    document.getElementById("barID").style.setProperty("--mouse-x-bar", `${x-40}px`)
+}
+const pagination = document.getElementById("paginationView")
+pagination.onmousemove = event => handleOnMouseMovePaginationBar(event)
+
+async function displayUpcomingMovies(page) {
+    const result = await getUncoming(page);
+    console.log(result);
+
+    const body = document.getElementById("upcomingMoviesContainer");
+    body.innerHTML = ""
+
+    document.getElementById("upcomingMoviesContainer").style.setProperty("--count", result.length)
+
+    for(let i = 0; i< result.length; i++){
+        const newCard = document.createElement("div");
+        newCard.setAttribute("onclick", `location.href='movieInformation.html?movieID=${result[i].id}'`)
+        newCard.classList.add("upcomingMovie");
+        var img = document.createElement('img');
+        if (result[i].backdrop_path === null) {
+            img.src = "./logosAndIcons/qualityLogo.png";
+            img.classList.add("upcomingMovieImage");
+        } else {
+            img.src = `${config["image_base_url"]  + result[i].poster_path}`;
+            img.classList.add("upcomingMovieImage");
+        }
+        newCard.appendChild(img);
+        var cardText = createText("h3", result[i].title)
+        newCard.appendChild(cardText);
+        body.appendChild(newCard);
+    }
+}
+
+var elements = document.getElementsByClassName("pagination-number");
+
+function getNumber(event) {
+    console.log(event.target.innerHTML)
+    const number = Number(event.target.innerHTML);
+    displayUpcomingMovies(number);
+  }
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', (e) => getNumber(e));
+}
